8b7cd0c5a9b6ce1687b4f41dd37bec17bb83b6f3
Jasa-Design.web.id <48776586+jasadesign-bdg@users.noreply.github.com>
2019-08-15 10:45:48 +0700

    Update wpLSN.js

--- TokoWhatsApp/wpLSN.js
+++ TokoWhatsApp/wpLSN.js
@@ -1,2 +1,2 @@
-var twLSN = ['www.cektokokita.com','cektokokita.com','tampilgaya.net','www.tampilgaya.net','localhost'];
+var twLSN = ['www.elvaro.us','elvaro.us','www.cektokokita.com','cektokokita.com','tampilgaya.net','www.tampilgaya.net','localhost'];
 var redirectURL = 'https://www.seotemplate.web.id';
